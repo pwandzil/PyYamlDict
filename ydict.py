4288f9f929d858b6bd1aa1d51294a3f923e2ca1f
Przemyslaw Wandzilak <4797644+pwandzil@users.noreply.github.com>
2022-05-12 22:05:56 +0200

    Rough shape

--- ydict.py
+++ ydict.py
@@ -3,12 +3,23 @@ import yaml, sys, collections
 if __name__ == '__main__':
 
     safe = False
+    sort = False
+
+    # Load
     if safe:
         stream = open("new.yaml", 'r')
         dictionary = yaml.safe_load(stream)
     else:
         dictionary = yaml.load(sys.stdin, Loader=yaml.FullLoader)
-    sorted_dictionary = sorted(dictionary.items(), key=lambda kv: kv[0])
-    
+
+    # Sort
+    sorted_dictionary = dictionary.items()
+    if sort:
+        sorted_dictionary = sorted(dictionary.items(), key=lambda kv: kv[0])
+
+    # Print
     for key, value in sorted_dictionary:
-        print (key + " : " + str(value))
+        if 'def' in value.keys():
+          print ("{}:\n  {}\n  {}{}\n".format(key, value['def'], str(value), type(value)))
+        else:
+          print ("{}:\n  {}, {}\n".format(key, str(value), type(value)))
